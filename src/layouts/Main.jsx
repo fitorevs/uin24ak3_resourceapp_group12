32165c67f503621b2eafdf92a9a95b05f439f3ef
NL-12 <nikol.lysebraate@hiof.no>
2024-03-05 13:27:39 +0100

    La til kommentarer i Main
    Kommentarer og mindre strukturmessig forandring med tanke på SASS

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom"
-import Nav from "../components/Nav"
+// Linje 2 til 3: Global og lokal importering.
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
 
+// Linje 6 til 14: Komponent / layout. Siden denne nettsiden ikke er så avansert strukturmessig er dette den eneste som er brukt. 
 export default function Main() {
-
+    // Linje 8 til 13: Returner et JSX objekt. Spesifikt et fragment med return-verdien fra to andre objekter. Nav er et egendefinert komponent. Mens Outlet er en del av routing. Med Outlet kommer React til å gjøre så komponent vi renderer blir brukt. Så i praksis vil alltid Nav være med på siden. Mens utifra hvilken side vi går til forandrer Outlet seg. 
     return (
-        <div id="container">
+        <>
             <Nav />
             <Outlet />
-        </div>
+        </>
     )
 }
